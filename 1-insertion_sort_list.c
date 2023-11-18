1060624c92a9744ad6bb9d2d56a6e91226d982c3
Abbey Kwame Morgan <constancekwameabbey@gmail.com>
2023-11-18 09:13:31 +0000

    Add print list source file

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -8,36 +8,30 @@
 
 void insertion_sort_list(listint_t **list)
 {
-    if (list == NULL || *list == NULL || (*list)->next == NULL)
-        return;
+	listint_t *current, *temp;
 
-    listint_t *current = (*list)->next;
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
+	current = (*list)->next;
 
-    while (current != NULL)
-    {
-        listint_t *temp = current->prev;
-        
-        while (temp != NULL && current->n < temp->n)
-        {
-            if (current->next != NULL)
-                current->next->prev = temp;
-
-            temp->next = current->next;
-            current->prev = temp->prev;
-            current->next = temp;
-
-            if (temp->prev != NULL)
-                temp->prev->next = current;
-            else
-                *list = current;
-
-            temp->prev = current;
-
-            temp = current->prev;
-            print_list(*list);
-        }
-
-        current = current->next;
-    }
+	while (current != NULL)
+	{
+		temp = current->prev;
+		while (temp != NULL && current->n < temp->n)
+		{
+			if (current->next != NULL)
+				current->next->prev = temp;
+			temp->next = current->next;
+			current->prev = temp->prev;
+			current->next = temp;
+			if (temp->prev != NULL)
+				temp->prev->next = current;
+			else
+				*list = current;
+			temp->prev = current;
+			temp = current->prev;
+			print_list(*list);
+		}
+		current = current->next;
+	}
 }
-
