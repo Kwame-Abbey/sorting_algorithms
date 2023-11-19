587ae5f7a45fe2ae99b5cc6da3bb90fc436f3b82
wahujos <jwahu30@gmail.com>
2023-11-19 20:14:13 +0000

    quick sort

---
+++ 3-quick_sort.c
@@ -0,0 +1,51 @@
+#include "sort.h"
+/**
+ * partition - This function does the partitioning of
+ * the array and also calls itself recursively.
+ * @array: This is the array that we need to sort
+ * @low_index: low index of the array to be sorted.
+ * @high_index: high index of the array that needs sorting
+ * @size: the size of the array.
+ */
+void partition(int *array, int low_index, int high_index, int size)
+{
+	int pivot, i, j, temp, temp2;
+
+	pivot = array[high_index];
+	i = low_index;
+	if (low_index < high_index)
+	{
+		for (j = low_index; j < high_index; j++)
+		{
+			if (array[j] < pivot)
+			{
+				if (j != low_index)
+				{
+					temp = array[j];
+					array[j] = array[i];
+					array[i] = temp;
+					print_array(array, size);
+				}
+				i++;
+			}
+		}
+		if (i != high_index)
+		{
+			temp2 = array[i];
+			array[i] = array[high_index];
+			array[high_index] = temp2;
+			print_array(array, size);
+		}
+	partition(array, low_index, (i - 1), size);
+	partition(array, (i + 1), high_index, size);
+	}
+}
+/**
+ * quick_sort - calls the partition function.
+ * @array: array to sort.
+ * @size: size of array.
+ */
+void quick_sort(int *array, size_t size)
+{
+	partition(array, 0, (size - 1), size);
+}
