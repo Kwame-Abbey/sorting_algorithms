8af31b1ef375872446850ade63baddd077e0db6a
wahujos <jwahu30@gmail.com>
2023-11-21 03:28:43 +0000

    update patition algorith

--- 107-quick_sort_hoare.c
+++ 107-quick_sort_hoare.c
@@ -22,13 +22,13 @@ void quick_sort_hoare(int *array, size_t size)
  */
 int hoare_partition(int *array, int low_index, int high_index, int size)
 {
-	int pivot, i, j, temp1, temp2;
+	int pivot, i, j, temp1;
 
 	pivot = array[high_index];
 	i = low_index;
 	j = high_index;
 
-	while (1)
+	for (i = low_index, j = high_index; 1; i++, j--)
 	{
 		while (array[i] < pivot)
 		{
@@ -42,20 +42,11 @@ int hoare_partition(int *array, int low_index, int high_index, int size)
 		{
 			return (i);
 		}
-		if (i < j)
-		{
 			temp1 = array[j];
 			array[j] = array[i];
 			array[i] = temp1;
 			print_array(array, size);
-		}
 	}
-	temp2 = array[pivot];
-	array[pivot] = array[i];
-	array[i] = temp2;
-	print_array(array, size);
-
-
 }
 /**
  * hoare_recursion - function responsible for the recursion
