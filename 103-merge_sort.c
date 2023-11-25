5cc439318d283b8fda29b7ad47cab2d2bb0a7760
wahujos <jwahu30@gmail.com>
2023-11-25 17:30:25 +0000

    changes

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -1,126 +1,101 @@
 #include "sort.h"
 /**
- * merge_sort - calls the function that deals with the division.
- * @array: array of integers
- * @size: size of the array
+ * merge - merge function. responsible for merging the array
+ * @array: the array that needs sorting
+ * @temp: temporary array. facilitates the merging
+ * @start: the begining of the array.
+ * @middle: middle index of the array
+ * @end: the end index of the array.
+ */
+void merge(int *array, int *temp, int start, int middle, int end)
+{
+	int i = start, j = middle, k = start, l = middle, flag = 0;
+
+	printf("Merging...\n");
+	printf("[left]: ");
+	while (k < middle)
+	{
+		if (flag)
+			printf(", ");
+		printf("%d", array[k++]);
+		flag = 1;
+	}
+	printf("\n");
+	flag = 0;
+	printf("[right]: ");
+	while (l < end)
+	{
+		if (flag)
+			printf(", ");
+		printf("%d", array[l++]);
+		flag = 1;
+	}
+	printf("\n");
+	k = start;
+	while (k < end)
+	{
+		if (i < middle && (j >= end || array[i] <= array[j]))
+			temp[k++] = array[i++];
+		else
+			temp[k++] = array[j++];
+	}
+	printf("[Done]: ");
+	k = start, flag = 0;
+	while (k < end)
+	{
+		if (flag)
+			printf(", ");
+		printf("%d", temp[k++]);
+		flag = 1;
+	}
+	printf("\n");
+}
+/**
+ * merge_recursive - responsible for recursive calls.
+ * @array: the array passed for sorting
+ * @start: the starting index of the array.
+ * @end: the end of the array.
+ * @temp: temporary array.
+ */
+void merge_recursive(int *array, int start, int end, int *temp)
+{
+	int middle = 0;
+
+	if (end - start <= 1)
+		return;
+	middle = (start + end) / 2;
+	merge_recursive(array, start, middle, temp);
+	merge_recursive(array, middle, end, temp);
+	merge(array, temp, start, middle, end);
+}
+/**
+ * merge_sort - calls the recursive function.
+ * @array: array passed for sorting
+ * @size: the size of the array.
  */
 void merge_sort(int *array, size_t size)
 {
 	int *temp;
+	size_t i = 0;
 
+	if (array == NULL || size < 2)
+		return;
 	temp = malloc(sizeof(int) * size);
 	if (temp == NULL)
 	{
 		exit(EXIT_FAILURE);
 	}
-	merge_sort_recursive(array, temp, 0, (size - 1));
+	while (i < size)
+	{
+		temp[i] = array[i];
+		i++;
+	}
+	merge_recursive(array, 0, size, temp);
+	i = 0;
+	while (i < size)
+	{
+		array[i] = temp[i];
+		i++;
+	}
 	free(temp);
-}
-/**
- * merge_sort_recursive - divides the array recursively.
- * @array: the array passed.
- * @temp: temporary array created
- * @left: left most index
- * @right: right most index
- */
-void merge_sort_recursive(int *array, int *temp, int left, int right)
-{
-	int mid;
-
-	if (left < right)
-	{
-		mid = (left + right) / 2;
-		merge_sort_recursive(array, temp, left, mid);
-		merge_sort_recursive(array, temp, (mid + 1), right);
-		merge(array, temp, left, mid, right);
-	}
-}
-/**
- * merge - performs the merging of the array
- * @array: array to sort
- * @temp: temporary array
- * @left: left most index
- * @mid: middle index
- * @right: right most index
- */
-void merge(int *array, int *temp, int left, int mid, int right)
-{
-	int i = left;
-	int j = mid + 1;
-	int k = left;
-	int x;
-
-	printf("Merging...\n");
-	print_left(array, left, mid);
-	print_right(array, mid, right);
-	while (i <= mid && j <= right)
-	{
-		if (array[i] <= array[j])
-			temp[k++] = array[i++];
-		else
-			temp[k++] = array[j++];
-	}
-	if (i > mid)
-	{
-		while (j <= right)
-		{
-			temp[k++] = array[j++];
-		}
-	}
-	else
-	{
-		while (i <= mid)
-		{
-			temp[k++] = array[i++];
-		}
-	}
-	printf("[Done]: ");
-	for (x = left; x <= right; x++)
-	{
-		array[x] = temp[x];
-		printf("%d", array[x]);
-		if (x < right)
-			printf(", ");
-	}
-	printf("\n");
-	/*print_done(array, left, right);*/
-}
-/**
- * print_left - focuses on printing
- * @array: the array to retrive the print
- * @left: left most index
- * @mid: middle index
- */
-void print_left(int *array, int left, int mid)
-{
-
-	int i;
-
-	printf("[Left]: ");
-	for (i = left; i <= mid; i++)
-	{
-		printf("%d", array[i]);
-		if (i < mid)
-			printf(", ");
-	}
-	printf("\n");
-}
-/**
- * print_right - focuses on printing
- * @array: the array to retrive the print
- * @mid: middle index
- * @right: right most index
- */
-void print_right(int *array, int mid, int right)
-{
-	int j;
-
-	printf("[right]: ");
-	for (j = mid + 1; j <= right; j++)
-	{
-		printf("%d", array[j]);
-		if (j < right)
-			printf(", ");
-	}
-	printf("\n");
 }
